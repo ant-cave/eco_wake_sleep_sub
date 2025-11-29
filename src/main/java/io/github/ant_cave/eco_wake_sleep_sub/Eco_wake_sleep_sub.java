78f3ae017bab0dde0411e2a5533d623714a1bbed
ant-cave <antmmmmmmmmmm@hotmail.com>
2025-11-29 22:38:16 +0800

    feat(eco_wake_sleep_sub): 实现状态查询功能并增强服务器交互
    - 添加状态枚举和相关变量追踪服务器状态
    - 实现玩家加入欢迎信息
    - 添加=status@命令查询当前插件状态
    - 更新plugin.yml文件，增加命令描述和使用说明

--- src/main/java/io/github/ant_cave/eco_wake_sleep_sub/Eco_wake_sleep_sub.java
+++ src/main/java/io/github/ant_cave/eco_wake_sleep_sub/Eco_wake_sleep_sub.java
@@ -1,17 +1,56 @@
 package io.github.ant_cave.eco_wake_sleep_sub;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Eco_wake_sleep_sub extends JavaPlugin {
+import java.util.logging.Logger;
+
+enum State {
+    WAKE,
+    WAKE_CONNECTED,
+    SLEEP
+}
+
+public final class Eco_wake_sleep_sub extends JavaPlugin implements Listener {
+
+    Logger logger = Bukkit.getLogger();
+    State currentStatus = State.SLEEP;//服务器连接状态
+    Integer timeToWait=0;
+
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
+        getCommand("status").setExecutor(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome to the server!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("status")) {
+            return commandStatus(sender);
+        }
+        return false;
+    }
+
+    boolean commandStatus(CommandSender sender){
+        sender.sendMessage("Current plugin state: " + currentStatus.name());
+        return true;
     }
 }
